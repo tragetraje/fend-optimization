d9d5f4b9f27e4cfe65864297880a1f7a07fbb105
ZAstafyeva <z.a.astafyeva@gmail.com>
2016-06-30 10:50:11 +1000

    replace querySelectorAll with getElementsByClassName

--- views/js/main.js
+++ views/js/main.js
@@ -501,7 +501,7 @@ function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
-  var items = document.querySelectorAll('.mover');
+  var items = document.getElementsByClassName('.mover');
   for (var i = 0; i < items.length; i++) {
     var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
