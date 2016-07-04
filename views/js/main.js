d9cdd0d6a153e3f1ad8bec22fc38adcc64f32b7e
ZAstafyeva <z.a.astafyeva@gmail.com>
2016-07-04 13:23:17 +1000

    Implement requestAnimationFrame for scrolling event

--- views/js/main.js
+++ views/js/main.js
@@ -529,7 +529,11 @@ function updatePositions() {
 }
 
 // runs updatePositions on scroll
-window.addEventListener('scroll', updatePositions);
+//window.addEventListener('scroll', updatePositions);
+
+window.addEventListener('scroll', function () {
+   requestAnimationFrame(updatePositions);
+});
 
 // Generates the sliding pizzas when the page loads.
 document.addEventListener('DOMContentLoaded', function() {
